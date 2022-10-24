0d38063036c83138c889ce2c315210801532f0a6
sazhvg <sazhvg@gmail.com>
2022-10-24 19:38:55 +0300

    task 5.5.2

--- spring-task/src/main/java/com/ua/sazhyn/volodymyr/springtask/jsoncontroller/JsonController.java
+++ spring-task/src/main/java/com/ua/sazhyn/volodymyr/springtask/jsoncontroller/JsonController.java
@@ -5,6 +5,10 @@ import com.ua.sazhyn.volodymyr.springtask.service.JsonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/json")
 public class JsonController {
@@ -19,32 +23,47 @@ public class JsonController {
 
 //projects – поверне список з назвами проектів які були збережені до цього
     @GetMapping("/projects")
-    public void getProjects() {
-        Data.getData().stream().filter((p) -> p.getProject() != null).forEach(p -> System.out.println(p.getProject()));
+    public Set<Data> getProjects() {
+        Set<Data> data = new HashSet<>();
+//        Data.getData().stream().filter((p) -> p.getProject() != null).forEach(p -> System.out.println(p.getProject()));
+        data = Data.getData().stream().filter((p) -> p.getProject() != null).collect(Collectors.toSet());
+        return data;
     }
 
 //failed-results – поверне список з результатами які не “success”
     @GetMapping("/failed-results")
-    public void getFailedResults() {
-        Data.getData().stream().filter((p) -> !p.getResult().equals("success")).forEach(p -> System.out.println(p.getProject()));
+    public Set<Data> getFailedResults() {
+        Set<Data> data = new HashSet<>();
+//        Data.getData().stream().filter((p) -> !p.getResult().equals("success")).forEach(p -> System.out.println(p.getProject()));
+        data = Data.getData().stream().filter((p) -> !p.getResult().equals("success")).collect(Collectors.toSet());
+        return data;
     }
 
 //long-compilation – поверне список з об’єктами в яких compilationTime >= 1000
     @GetMapping("/long-compilation")
-    public void getLongCompilation() {
-        Data.getData().stream().filter((p) -> p.getCompilationTime() >= 1000).forEach(p -> System.out.println(p.getProject()));
+    public Set<Data> getLongCompilation() {
+        Set<Data> data = new HashSet<>();
+//        Data.getData().stream().filter((p) -> p.getCompilationTime() >= 1000).forEach(p -> System.out.println(p.getProject()));
+        data = Data.getData().stream().filter((p) -> p.getCompilationTime() >= 1000).collect(Collectors.toSet());
+        return data;
     }
 
 //normal -compilation – поверне список з об’єктами в яких compilationTime < 1000
     @GetMapping("/normal -compilation")
-    public void getNormalCompilation() {
-        Data.getData().stream().filter((p) -> p.getCompilationTime() < 1000).forEach(p -> System.out.println(p.getProject()));
+    public Set<Data> getNormalCompilation() {
+        Set<Data> data = new HashSet<>();
+//        Data.getData().stream().filter((p) -> p.getCompilationTime() < 1000).forEach(p -> System.out.println(p.getProject()));
+        data = Data.getData().stream().filter((p) -> p.getCompilationTime() < 1000).collect(Collectors.toSet());
+        return data;
     }
 
 //success-result - – поверне список з результатами які “success”
     @GetMapping("/success-result")
-    public void getSuccessResults() {
-        Data.getData().stream().filter((p) -> p.getResult().equals("success")).forEach(p -> System.out.println(p.getProject()));
+    public Set<Data> getSuccessResults() {
+        Set<Data> data = new HashSet<>();
+//        Data.getData().stream().filter((p) -> p.getResult().equals("success")).forEach(p -> System.out.println(p.getProject()));
+        data = Data.getData().stream().filter((p) -> p.getResult().equals("success")).collect(Collectors.toSet());
+        return data;
     }
 }
 /*	Створи простий веб-сервіс, який приймає json (за допомогою Postman) такого вигляду:
